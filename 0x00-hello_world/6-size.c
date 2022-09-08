cb2a3e957349fb68ab526afbe8cc601a688396ce
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-08 15:06:12 -0700

    ff

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+  printf("Size of the integer is:%d bytes\n",sizeof(int));
+  printf("Size of the char is :%i bytes\n",sizeof(char));
+  printf("Size of the long int:%d bytes\n",sizeof(long int));
+  printf("Size of the long long int:%d bytes\n",sizeof(long long int));
+  printf("Size of the float:%d bytes\n",sizeof(float));
+  return(0);
+}
