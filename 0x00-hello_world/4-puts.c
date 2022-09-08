5fac035cb37e79a5afec269fd0132bd20268f7c4
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-08 13:58:14 -0700

    kk

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints a string
+ */
+int main(void)
+{
+  char *s= "Programming is like building a multilingual puzzle";
+  puts(s);
+  return (0)
+}
