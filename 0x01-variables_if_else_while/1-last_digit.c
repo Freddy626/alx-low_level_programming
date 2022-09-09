8278a2337cd7b91080b99f99ed8a42e53d62b0b7
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-09 11:57:26 -0700

    ff

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - conditional statement for two integers
+ * Description: puts two integers in one printf statement
+ * Return: 0
+ */ 
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int a = n % 10;
+ if (a >5)
+{
+printf("Last digit of %d is %d and is greater than 5", n, a);
+}
+else if (a = 0)
+{
+printf("Last digit of %d is %d and is zero", n, a);
+}
+else if (a < 6)
+{
+printf("Last digit of %d is %d and is less than 6 not 0", n, a);
+}
+else
+{
+putchar('\n');
+}
+return (0);
+}
+
