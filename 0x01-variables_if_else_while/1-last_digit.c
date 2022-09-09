4b8dfc8f3fbf8ddb10cb3287b966c476661bb114
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-09 12:17:27 -0700

    ff

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
  * main - conditional statement for two integers
  * Description: puts two integers in one printf statement
  * Return: 0
- */ 
+ */
 int main(void)
 {
 int n, a;
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 a = n % 10;
 if (a > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, a);
+printf("Last digit of %d is %d and is greater than 5\n", n, a);
 }
 else if (a == 0)
 {
-printf("Last digit of %d is %d and is zero", n, a);
+printf("Last digit of %d is %d and is zero\n", n, a);
 }
 else if (a < 6)
 {
-printf("Last digit of %d is %d and is less than 6 not 0", n, a);
+printf("Last digit of %d is %d and is less than 6 not 0\n", n, a);
 }
 else
 {
