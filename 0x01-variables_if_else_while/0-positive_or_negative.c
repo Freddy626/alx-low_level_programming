509cc6f1b153c0a0ebd2e2fd165a5c750b654418
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-09 10:48:17 -0700

    hh

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,19 +11,21 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n>0)
+if (n > 0)
 {
-printf("%d is positive", n );
+printf("%d is positive\n", n );
 }
-else if(n==0)
+else if (n == 0)
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
-else if(n<0)
+else if (n < 0)
 {
-  printf("%d is negative", n);
-} else {
-  printf("/n");
-     }  
+printf("%d is negative\n", n);
+}
+else
+{
+printf("/n");
+}  
 return (0);
 }
