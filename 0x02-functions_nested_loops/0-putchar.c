eade603b4b06d64cfe1e0846adfa91f52c5c2172
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-13 09:13:10 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - function that prints _putchar
+ *Description: function that prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+printf("_putchar\n");
+return(0);
+}
