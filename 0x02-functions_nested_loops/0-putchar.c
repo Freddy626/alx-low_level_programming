8bd5b5f898fd38b730b7b61ecd7057e23846aca0
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-13 12:26:21 -0700

    jj

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include"main.h"
 /**
- * main - function that prints _putchar
- *Description: function that prints _putchar
+ * main - Entry point
+ *
  * Return: 0
  */
 int main(void)
 {
-printf("_putchar\n");
+char word [8] = "_putchar";
+int i;
+for(i = 0; i < 8; i++)
+_putchar(word[i]);
+_putchar('\n');
 return(0);
 }
