7fcf0ab2df1d5d353a6a91766cf98868b138e864
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-13 16:49:08 -0700

    ff

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.c"
+/**
+ * print_last_digit - prints las digit
+ * @n: value
+ *Return: last_digit
+ */
+int print_last_digit(int n)
+{
+int last_digit;
+last_digit = n % 10;
+if (last_digit < 0)
+{
+_putchar('0' + -last_digit);
+return (-last_digit);
+}
+else
+{
+_putchar('0' + last_digit);
+return (last_digit);
+}
+}
