1b9dcceb22143bb3f4424ce68fbc43ea2cc10532
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-13 13:48:21 -0700

    ff

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower -lower case alphabet
+ * @c: value
+ * Description: checks
+ * Return: 1 or 0
+ */
+int _islower(intc)
+{
+if (islower(c) > 0)
+return (1);
+else
+return (0);
+}
