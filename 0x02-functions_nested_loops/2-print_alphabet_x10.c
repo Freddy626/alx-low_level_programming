a5887cad70d323541508d3756779ed8fb249227e
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-13 13:20:13 -0700

    ff

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include"main.h"
 /**
- * main - print 10 times
+ * print_alphabet_x10 - print 10 times
  *
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
 int a, j = 0;
-while(j<10)
+ while (j < 10)
 {
 for (a = 'a'; a <= 'z'; a++)
 _putchar(a);
