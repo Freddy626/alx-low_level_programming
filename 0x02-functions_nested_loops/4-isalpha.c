f631feb0282f601ff6470f6d981c8786e11528a8
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-13 14:10:27 -0700

    ff

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabets
+ * @c: value
+ * Description: checks
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+if (isalpha(c) > 0)
+return (1);
+else
+return (0);
+}
