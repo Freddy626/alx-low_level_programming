6b4b3553f34d0367febdfcafb8759c83a5503163
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-13 14:25:28 -0700

    ff

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_sign - print that shit
+ * @n: value
+ * Description: checks
+ * Return: 1 0r 0 or -1
+ */
+int print_sign(int n)
+{
+if (print_sign(n) > 0)
+_putchar('+');
+return (1);
+else if (print_sign(n) = 0)
+_putchar('0');
+return (0);
+else if (print_sign(n) < 0)
+ _putchar('-');
+ return (-1);
+}
