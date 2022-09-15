8fe828746b1bd3a1d74c82826a851323906f6f71
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-15 09:37:27 -0700

    ff

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _isupper(int c);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+#endif
