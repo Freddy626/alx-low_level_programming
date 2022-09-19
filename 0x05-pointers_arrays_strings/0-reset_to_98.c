78007a47df6112ca0f0644ac39ad6675c430f1be
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-19 12:18:29 -0700

    ff

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ *reset_to_98 - takes parameter and resets to 98
+ * @n: variable to be reset
+ * Return: nothing
+ */
+int main(void)
+{
+  void reset_to_98(int *n)
+    
+  {
+    
+    *n = 98;
+    
+  }
+  return;
+}
