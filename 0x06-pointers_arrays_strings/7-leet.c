3bceae65b1e9eaaa8ef9547a0f01cbb40a97c82c
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-22 21:53:17 -0700

    ff

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,10 +11,10 @@ int b;
 int l = 5;
 char tr[5] = { 'A', 'E', 'O', 'T', 'L'};
 char trw[5] = { '4', '3', '0', '7', '1'};
-while (s[a])
+while(s[a])
 {
 b = 0;
-while (b < l)
+while(b < l)
 {
 if(s[a] == tr[b] || s[a] - 32 == tr[b])
 s[a] = trw[b];
