94198b49f533c2c8ff53431e44ec71abeaa16204
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-22 21:48:29 -0700

    dd

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,26 @@
 #include "main.h"
 /**
  * leet - a leet function
- * @x - parameter
+ * @s - parameter
  * Return: string
  */
-char *leet(char *x)
+char *leet(char *s)
 {
-int a = 0; b; l = 5
+int a = 0;
+int b;
+int l = 5;
 char tr[5] = { 'A', 'E', 'O', 'T', 'L'};
 char trw[5] = { '4', '3', '0', '7', '1'};
-while (x[a])
+while (s[a])
 {
 b = 0;
 while (b < l)
 {
-if(x[a] == tr[b] || x[a] - 32 == tr[b])
-x[a] = trw[b];
+if(s[a] == tr[b] || s[a] - 32 == tr[b])
+s[a] = trw[b];
 b++;
 }
 a++;
 }
-return(x);
+return(s);
 }
