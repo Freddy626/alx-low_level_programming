6e183de8bed84fd460a1a9f223ad0cbc40e0993b
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-09-26 13:17:32 -0700

    ff

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,7 +3,7 @@
  * print_chessboard - Prints a chessboard.
  * @a: The chessboard to be printed.
  */
-void print_chessboard(char (*a)[8]);
+void print_chessboard(char (*a)[8])
 {
 int indx1, indx2;
 for (indx1 = 0; a[indx1][7]; indx1++)
