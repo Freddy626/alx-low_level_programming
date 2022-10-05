8393052bf864abd74614d0a7fa585e382ea407f9
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-10-05 11:54:14 -0700

    gg

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isalpha - checks for alphabets
+ * @c: value
+ * Description: checks
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+if (isalpha(c) > 0)
+return (1);
+else
+return (0);
+}
